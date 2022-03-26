adec47c5ec71ef3e3126f3ca3dfdf3626ab05dd6
mvCoder-ux <82150372+mvCoder-ux@users.noreply.github.com>
2022-03-26 16:44:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import time
+
+
 
 while True: 
     choices = ['rock', 'paper', 'scissors']
@@ -10,41 +13,55 @@ while True:
         player_choice = input('Rock, Paper or Scissors?: ').lower()
     
     if player_choice == computer_choice:
-        print('Computer Choice: ', computer_choice)
-        print('Player Choice: ', player_choice)
+        print('Computer is thinking....')
+        time.sleep(3)
+        print('🖥️ Computer Choice: ', computer_choice)
+        print('🙍 Player Choice: ', player_choice)
         print('Tie! No one wins 🤝')
 
     elif player_choice == 'rock':
         if computer_choice == 'paper':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: 📄', computer_choice)
+            print('🙍 Player Choice: 🪨 ', player_choice)
             print('You lose!! 👎')
 
         if computer_choice == 'scissors':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: ✂️ ', computer_choice)
+            print('🙍 Player Choice: 🪨 ', player_choice)
             print('You win!! 👍')
 
     elif player_choice == 'paper':
         if computer_choice == 'rock':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: 🪨 ', computer_choice)
+            print('🙍 Player Choice: 📄', player_choice)
             print('You win!! 👍')
 
         if computer_choice == 'scissors':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: ✂️ ', computer_choice)
+            print('🙍 Player Choice: 📄', player_choice)
             print('You lose!! 👎')
 
     elif player_choice == 'scissors':
         if computer_choice == 'rock':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: 🪨 ', computer_choice)
+            print('🙍 Player Choice: ✂️ ', player_choice)
             print('You lose!! 👎')
 
         if computer_choice == 'paper':
-            print('Computer Choice: ', computer_choice)
-            print('Player Choice: ', player_choice)
+            print('Computer is thinking....')
+            time.sleep(3)
+            print('🖥️  Computer Choice: 📄', computer_choice)
+            print('🙍 Player Choice: ✂️ ', player_choice)
             print('You win!! 👍')
 
     play_again = input('Want to play again? (Yes/No): ').lower()
